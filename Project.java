e0915c85d8fefe7215eb47f828935333e0763b4c
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2023-10-08 18:44:28 -0600

    Update Project.java

---
+++ Project.java
@@ -0,0 +1,49 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+    private String titulo;
+    private String descripcion;
+    private String fechaVencimiento;
+    private List<Tarea> tareas;
+
+    public Proyecto(String titulo, String descripcion, String fechaVencimiento) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaVencimiento = fechaVencimiento;
+        this.tareas = new ArrayList<>();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public List<Tarea> getTareas() {
+        return tareas;
+    }
+
+    public void setTareas(List<Tarea> tareas) {
+        this.tareas = tareas;
+    }
+}
